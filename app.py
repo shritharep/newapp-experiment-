25774c4f5feb7db9eacb275407a663c6cc751c51
Shritha Repala <shritharepala@gmail.com>
2025-08-08 18:48:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import google.generativeai as genai
+
 import first
 import second
+import third
+
+
 st.set_page_config(page_title="FreshFetch", layout="wide")
 
 st.markdown("Choose your personal guide for today")
 
-firstchoice, secondchoice = st.columns(2)
+firstchoice, secondchoice, thirdchoice = st.columns(3)
 
 if "selected_app" not in st.session_state:
   st.session_state.selected_app = "Individual Wellness Trainer"
@@ -17,8 +21,15 @@ with firstchoice:
 with secondchoice:
   if st.button("360° Status Prep"):
     st.session_state.selected_app = "360° Status Prep"
+with thirdchoice:
+  if st.button("In-Depth Meal Planner"):
+    st.session_state.selected_app = "In-Depth Meal Planner"
+
 
 if st.session_state.selected_app == "Individual Wellness Trainer":
   first.run()
 elif st.session_state.selected_app == "360° Status Prep":
   second.run()
+elif st.session_state-selected_app == "In-Depth Meal Planner":
+  third.run()
+
